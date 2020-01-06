886c8c0360f2701910588b4727d9ae16aed8c2d1
coadyi3 <ian.coady3@mail.dcu.ie>
2020-01-06 11:14:43 +0000

    Added Shared preferences and save button functionality.

--- app/src/main/java/com/example/sdaassign4_2019/Settings.java
+++ app/src/main/java/com/example/sdaassign4_2019/Settings.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -22,8 +27,9 @@ public class Settings extends Fragment {
 
     private static final String NAME_KEY = "NAME_KEY";
     private static final String EMAIL_KEY = "EMAIL_KEY";
-    private static final int ID_KEY = 0;
+    private static final String ID_KEY = "ID_KEY";
     private TextView name, email, id;
+    private EditText validName, validEmail, validId;
 
     public Settings() {
         // Required empty public constructor
@@ -38,13 +44,65 @@ public class Settings extends Fragment {
 
         final SharedPreferences prefs = this.getActivity().getSharedPreferences("preferences", MODE_PRIVATE );
 
-        name = root.findViewById(R.id.savedName);
-        name.setText(prefs.getString(NAME_KEY, ""));
-        
+        name    = root.findViewById(R.id.savedName);
+        email   = root.findViewById(R.id.savedEmail);
+        id      = root.findViewById(R.id.savedId);
 
+        name.setText(prefs.getString(NAME_KEY, ""));
+        email.setText(prefs.getString(EMAIL_KEY, ""));
+        id.setText(String.valueOf(prefs.getInt(ID_KEY, 0)));
+
+        if(Integer.parseInt(id.getText().toString()) == 0) id.setVisibility(View.INVISIBLE);
+
+
+        validName = root.findViewById(R.id.userName);
+        validEmail = root.findViewById(R.id.email);
+        validId = root.findViewById(R.id.borrowerID);
+
+        final Button saveBtn = root.findViewById(R.id.saveButton);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tempName = validName.getText().toString();
+                String tempEmail = validEmail.getText().toString();
+                int tempId;
+
+                if(validId.getText().toString().equals("")){ tempId = 0;}
+                else { tempId = Integer.parseInt(validId.getText().toString());}
+
+                if (tempName.length() > 2 && isValidEmail(tempEmail) && (tempId > 10000000 && tempId < 99999999)) {
+
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString(NAME_KEY, tempName);
+                    editor.putString(EMAIL_KEY, tempEmail);
+                    editor.putInt(ID_KEY, tempId);
+                    editor.apply();
+
+                    name.setText(tempName);
+                    email.setText(tempEmail);
+                    id.setText(String.valueOf(tempId));
+
+                    id.setVisibility(View.VISIBLE);
+
+                }
+
+                else{
+                    Toast.makeText(getContext(), "Please enter a valid name, email address and/or borrower ID!", Toast.LENGTH_SHORT).show();
+
+                    validName.setText("");
+                    validEmail.setText("");
+                    validId.setText("");
+                }
+
+            }
+        });
 
         return root;
 
+    }
+
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 
 
